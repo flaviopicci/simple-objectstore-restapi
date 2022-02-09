da237ac7cd06a9e1198be852f2b2c97794c09536
Flavio Piccinelli <fpiccinelli@jmawireless.com>
2022-02-09 15:20:21 +0100

    Fixed http logging middleware

--- internals/rest/router.go
+++ internals/rest/router.go
@@ -22,8 +22,8 @@ func NewRouter(s ObjectStore, maxMem int64, l *log.Logger) http.Handler {
 		store:  s,
 		maxMem: maxMem,
 	}
-	r.HandleFunc("/{bucket:[a-z0-9_-]+}/{objectId:[a-z0-9_-]+}", h.HandleRetrieve).Methods("GET")
 	r.HandleFunc("/{bucket:[a-z0-9_-]+}/{objectId:[a-z0-9_-]+}", h.HandleStore).Methods("PUT")
+	r.HandleFunc("/{bucket:[a-z0-9_-]+}/{objectId:[a-z0-9_-]+}", h.HandleRetrieve).Methods("GET")
 	r.HandleFunc("/{bucket:[a-z0-9_-]+}/{objectId:[a-z0-9_-]+}", h.HandleDelete).Methods("DELETE")
 
 	return r
@@ -31,11 +31,29 @@ func NewRouter(s ObjectStore, maxMem int64, l *log.Logger) http.Handler {
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
+		lrw := NewLoggingResponseWriter(w)
+		next.ServeHTTP(lrw, r)
+
+		statusCode := lrw.statusCode
+
 		level := log.DebugLevel
-		if r.Response.StatusCode >= http.StatusBadRequest {
+		if statusCode >= http.StatusBadRequest {
 			level = log.WarnLevel
 		}
-		logger.Logf(level, "%s %d", r.RequestURI, r.Response.StatusCode)
+		logger.Logf(level, "%d %s %s", statusCode, r.Method, r.URL.Path)
 	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
